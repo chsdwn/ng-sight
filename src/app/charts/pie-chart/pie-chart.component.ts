d5d627a409535caba3bc0fab9c6e0b12f09ff221
koniia <hulusikafalier@gmail.com>
2019-05-02 19:27:32 +0300

    Added sample pie chart.

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-pie-chart',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  // Pie
+  public pieChartLabels: string[] = ['Download', 'Upload', 'Mail Sales'];
+  public pieChartData: number[] = [300, 500, 100];
+  public pieChartType = 'doughnut';
+  public colors: any[] = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+      borderColor: '#111'
+    },
+  ];
 
   constructor() { }
 
